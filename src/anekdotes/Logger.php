775eec9b6c79b0443716172d53e4c838c9ec5af7
Steve Gagné <steve.gagne@anekdotes.com>
2016-04-28 09:19:24 -0400

    Added general structure

---
+++ src/anekdotes/Logger.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Logger package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Anekdotes\Logger;
+
+use Anekdotes\Logger\Logger;
+
+/**
+ * Dynamic representation of the Logger
+ */
+class Logger{
+
+}
---
+++ tests/LogTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Logger package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests;
+
+use Anekdotes\Logger\Log;
+use PHPUnit_Framework_TestCase;
+
+class LogTest extends PHPUnit_Framework_TestCase
+{
+  
+}
